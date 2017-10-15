152865de11277f5ede5ea5adc5efda30879c99e1
erin johnson <erin.nicole.johnson@gmail.com>
2017-10-15 09:45:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var updateObjectWithKeyAndValue = function(object,key,value) {
   return Object.assign({}, object, { [key]: value })
 }
 
-var destructivelyUpdateObjectWitKeyAndValue = function(object, key, value) {
+var destructivelyUpdateObjectWithKeyAndValue = function(object, key, value) {
   new Object(object[key]= value);
   return object
 }
